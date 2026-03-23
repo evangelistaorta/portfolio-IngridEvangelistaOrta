06de8452cfaa607ea5fac05f2b777299650913aa
evangelistaorta <evangelistaorta@hotmail.com>
2026-03-23 13:33:23 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -3,3 +3,14 @@ fetch("navbar.html")
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   });
+
+document.addEventListener("click", function (e) {
+  const toggle = document.getElementById("menu-toggle");
+  const links = document.getElementById("nav-links");
+
+  if (toggle && links) {
+    toggle.addEventListener("click", () => {
+      links.classList.toggle("active");
+    });
+  }
+});
